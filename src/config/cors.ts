ea0fa9d087ffe06ad8d0c50a747905ef59ae3d18
Omkar <omkarak45@gmail.com>
2021-11-15 16:36:59 +0530

    add: domain in cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,7 +1,11 @@
 import { CorsOptions } from 'cors'
 
 export const corsOptions: CorsOptions = {
-	origin: ['http://localhost:3000', 'https://dogemart.vercel.app'],
+	origin: [
+		'http://localhost:3000',
+		'https://dogemart.vercel.app',
+		'https://dogemart.up.railway.app',
+	],
 	optionsSuccessStatus: 200,
 	credentials: true,
 	exposedHeaders: ['set-cookie'],
